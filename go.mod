02e3550208668d42728646b26d105878c23c1338
AmarnathCJD <amanathcharichilil@gmail.com>
2022-03-01 03:39:36 -0800

    ss

---
+++ go.mod
@@ -0,0 +1,17 @@
+module main
+
+go 1.17
+
+require github.com/xelaj/mtproto v1.0.0
+
+require (
+	github.com/fatih/structtag v1.2.0 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xelaj/errs v0.0.0-20200831133608-d1c11863e019 // indirect
+	github.com/xelaj/go-dry v0.0.0-20201104165138-61a25872c05a // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/sys v0.0.0-20201029080932-201ba4db2418 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/xelaj/mtproto/telegram"
+)
+
+var (
+	appHash     = os.Getenv("APP_HASH")
+	Token       = os.Getenv("TOKEN")
+	appID, _    = strconv.Atoi(os.Getenv("APP_ID"))
+	sessionFile = filepath.Join("./", "session.json")
+	publicKeys  = filepath.Join("./", "tg_public_keys.pem")
+)
+
+func main() {
+	c, err := telegram.NewClient(telegram.ClientConfig{
+		SessionFile:     sessionFile,
+		PublicKeysFile:  publicKeys,
+		ServerHost:      "149.154.167.50:443",
+		AppID:           appID,
+		DeviceModel:     "Unknown",
+		SystemVersion:   "windows/amd64",
+		AppHash:         appHash,
+		InitWarnChannel: true,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err2 := c.AuthImportBotAuthorization(1, int32(appID), appHash, Token)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+}
