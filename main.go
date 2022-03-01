7e2986d66e508161d3501fafaa85b8d428361c75
AmarnathCJD <amanathcharichilil@gmail.com>
2022-03-01 03:50:10 -0800

    ss

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strconv"
 
 	"github.com/xelaj/mtproto/telegram"
@@ -13,8 +12,8 @@ var (
 	appHash     = os.Getenv("APP_HASH")
 	Token       = os.Getenv("TOKEN")
 	appID, _    = strconv.Atoi(os.Getenv("APP_ID"))
-	sessionFile = filepath.Join("./", "session.json")
-	publicKeys  = filepath.Join("./", "tg_public_keys.pem")
+	sessionFile = "session.json"
+	publicKeys  = "tg_public_keys.pem"
 )
 
 func main() {
@@ -31,9 +30,5 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	_, err2 := c.AuthImportBotAuthorization(1, int32(appID), appHash, Token)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-	fmt.Println("Hi there!")
+	fmt.Println("Hi there!", c)
 }
