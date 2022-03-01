08cef93068fd184b585a6e6867f7fee7fcff49bf
AmarnathCJD <amanathcharichilil@gmail.com>
2022-03-01 03:57:02 -0800

    xx

--- main.go
+++ main.go
@@ -11,12 +11,22 @@ import (
 )
 
 var (
-	appHash     = os.Getenv("APP_HASH")
-	Token       = os.Getenv("TOKEN")
-	appID, _    = strconv.Atoi(os.Getenv("APP_ID"))
-	sessionFile = "session.json"
-	publicKeys  = "tg_public_keys.pem"
+	appHash  = os.Getenv("APP_HASH")
+	Token    = os.Getenv("TOKEN")
+	appID, _ = strconv.Atoi(os.Getenv("APP_ID"))
 )
+
+const (
+	// from https://my.telegram.org/apps
+	TgAppID      = 8950585              // integer value from "App api_id" field      // string value from "App api_hash" field
+	TgTestServer = "149.154.167.40:443" // string value from "Test configuration" field
+	TgProdServer = "149.154.167.50:443" // string value from "Production configuration" field
+
+	TgBotUserName = "aiko_robot" // username of the bot
+)
+
+var TgAppHash = os.Getenv("APP_HASH")
+var TgBotToken = os.Getenv("TOKEN")
 
 func main() {
 	appStorage := utils.PrepareAppStorageForExamples()
