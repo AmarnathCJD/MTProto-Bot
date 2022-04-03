ad4119b6a31b0145f1e61015d6a3e803560e1b16
Amarnath C <amarnathcharichilil@gmail.com>
2022-04-03 13:11:47 +0530

    Update main.go

--- main.go
+++ main.go
@@ -50,7 +50,7 @@ func main() {
         if errx != nil {
 		fmt.Println(errx)
 	}
-	uname, err := client.ContactsResolveUsername(botUsername)
+	uname, err := c.ContactsResolveUsername("roseloverx")
 	if err != nil {
 		fmt.Println("ResolveUsername error:", err.Error())
 		os.Exit(1)
